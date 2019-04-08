7017543a0c4dc269766633026436f0d072c3875f
Aseu <felaugmar@gmail.com>
2019-04-08 20:08:43 -0300

    ✨ Character moving on the plane

---
+++ Assets/FurbEletivaVR/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class PlayerController : MonoBehaviour
+{
+    public AICharacterControl characterControl;
+
+    private void Awake()
+    {
+        if (characterControl == null)
+            characterControl = GetComponentInChildren<AICharacterControl>();
+    }
+}
