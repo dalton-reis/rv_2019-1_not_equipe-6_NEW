7f271283702d9a1d87cfeb84926c7e5c1a243c9f
Aseu <felaugmar@gmail.com>
2019-05-27 16:55:48 -0300

    ✨ Different congratulations texts and Fireworks sound effects

--- Assets/FurbEletivaVR/Scripts/ShelterArrival.cs
+++ Assets/FurbEletivaVR/Scripts/ShelterArrival.cs
@@ -6,6 +6,7 @@ using System;
 
 using Random = UnityEngine.Random;
 using UnityEngine.UI;
+using System.Collections;
 
 public class ShelterArrival : MonoBehaviour
 {
@@ -16,6 +17,12 @@ public class ShelterArrival : MonoBehaviour
 
     public float MinTrigger = 0.5f;
     public float MaxTrigger = 0.8f;
+
+    public float MinSecondsDelayFireworks = 1.0f;
+    public float MaxSecondsDelayFireworks = 1.5f;
+
+    public AudioClip FireworksLaunchAudio;
+    public AudioClip FireworksExplodeAudio;
 
     private bool trigger = false;
 
@@ -38,16 +45,63 @@ public class ShelterArrival : MonoBehaviour
     {
         var gameManager = GameManager.Instance;
         var ok = gameManager.Ok;
-        CongratulationsBoardText.text = "<color=#e03d1f><b>Parabéns!</b></color>\n" +
-            $"Você salvou {ok} das {ok + gameManager.Fail} pessoas!";
+        float total = ok + gameManager.Fail;
 
-        foreach (var particle in ParticlesToPlay)
-            particle.Play(true);
+        float okRatio = ok / total;
+
+        if (okRatio < 0.5f)
+        {
+            // Saved a few people
+            CongratulationsBoardText.text = "<color=#e03d1f><b>Poxa</b></color>\n" +
+                $"Você só conseguiu salvar {ok} das {total} pessoas!";
+        }
+        else if (okRatio < 0.9f)
+        {
+            // Saved almost everyone
+            CongratulationsBoardText.text = "<color=#e03d1f><b>Muito bom</b></color>\n" +
+                $"Você conseguiu salvar {ok} das {total} pessoas!";
+        }
+        else if (okRatio < 1.1f)
+        {
+            // Saved everyone
+            CongratulationsBoardText.text = "<color=#e03d1f><b>Parabéns!</b></color>\n" +
+                $"Você salvou todas as {total} pessoas!";
+        }
+
+        StartCoroutine(StartEndlessFireworks());
     }
 
     private void PeopleWave()
     {
         foreach (var person in peopleToAnimate)
             StartCoroutine(person.Play(Random.Range(MinTrigger, MaxTrigger)));
+    }
+
+    private IEnumerator StartEndlessFireworks()
+    {
+        var fireworksCount = ParticlesToPlay.Count;
+
+        if (fireworksCount == 0)
+            yield break;
+
+        while (true)
+        {
+            var fireworkToPlay = ParticlesToPlay[Random.Range(0, fireworksCount)];
+            var fireworkToPlayAudioSource = fireworkToPlay.gameObject.GetComponent<AudioSource>();
+
+            fireworkToPlay.Play();
+
+            // Play Launch
+            fireworkToPlayAudioSource.PlayOneShot(FireworksLaunchAudio);
+            // Wait
+            yield return new WaitForSeconds(FireworksLaunchAudio.length);
+            // Play Explosion
+            fireworkToPlayAudioSource.PlayOneShot(FireworksExplodeAudio);
+
+            var fireworksSecondsDelay = Random.Range(MinSecondsDelayFireworks, MaxSecondsDelayFireworks);
+
+            // Wait before playing again
+            yield return new WaitForSeconds(FireworksLaunchAudio.length + fireworksSecondsDelay);
+        }
     }
 }
