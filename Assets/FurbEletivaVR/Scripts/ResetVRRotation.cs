3e8fb94509df15daef48d1800ed1ef8ca1a07d56
Aseu <felaugmar@gmail.com>
2019-06-03 17:19:14 -0300

    ✨ Water and Map

---
+++ Assets/FurbEletivaVR/Scripts/ResetVRRotation.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class ResetVRRotation : MonoBehaviour
+{
+    void Update()
+    {
+        ResetVR();
+    }
+
+    void ResetVR()
+    {
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+    }
+}
